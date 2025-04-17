19112345c73a33a789040ae70c33e89d4257c515
cuongtrong193 <cuongmanucian19@gmail.com>
2025-04-17 15:33:17 +0700

    Rearrange file order by template

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './part1/unicafe/App'
+import App from './part1/unicafe/src/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
